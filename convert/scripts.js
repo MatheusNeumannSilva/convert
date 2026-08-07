24fa82b5aa9a948c76f4a0c3c748bf1db3e11c71
neumannsilva23-creator <neumannsilva23@gmail.com>
2026-08-07 08:54:47 -0300

    first commit

--- convert/scripts.js
+++ convert/scripts.js
@@ -1,1 +1,6 @@
 const amount = document.getElementById("amount");
+
+// Manipulando o input amount para receber somente números
+amount.addEventListener("input", () => {
+  console.log(amount.value);
+});
